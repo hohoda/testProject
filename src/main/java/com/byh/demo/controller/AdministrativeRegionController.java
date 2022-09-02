8ec48d865a7bdab962b7a23a8096582d698d1db8
623412301@qq.com <623412301@qq.com>
2022-09-02 17:45:16 +0800

    升级jdk版本从13到17。

--- src/main/java/com/byh/demo/controller/AdministrativeRegionController.java
+++ src/main/java/com/byh/demo/controller/AdministrativeRegionController.java
@@ -1,5 +1,9 @@
 package com.byh.demo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.byh.demo.util.JsonUtil;
+import io.lettuce.core.api.reactive.RedisHashReactiveCommands;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +14,14 @@ public class AdministrativeRegionController {
 
     @GetMapping("/test")
     public String test(){
-        return "11111";
+        //返回的json
+            JSONArray retust=new JSONArray();
+            //解析全部数据
+            String path = JsonUtil.class.getClassLoader().getResource("static/china_city_4_class.json").getPath();
+            String jsonStr = JsonUtil.readJsonFile(path);
+            JSONArray jobj = JSON.parseArray(jsonStr);
+            JSONArray j1=new JSONArray();
+        return jsonStr;
     }
 
 }
